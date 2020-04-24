020d9f76aba207c2c0800e15f92d81f063f279dc
Daniel Polo <od.polo@gmail.com>
2020-04-23 20:19:54 -0500

    fix(setup): Deleted travis CI file & batch 380 init

--- test/lewagon/sample_test.rb
+++ test/lewagon/sample_test.rb
@@ -12,18 +12,4 @@ class SampleTest < Test::Unit::TestCase
     # assert LeWagon::Address.city.is_a? String
     # assert LeWagon::Address.street_name.is_a? String
   end
-
-  def test_zh_tw_name_methods
-    # assert LeWagon::Name.last_name.is_a? String
-    # assert LeWagon::Name.first_name.is_a? String
-    # assert LeWagon::Name.name.is_a? String
-    # assert LeWagon::Name.name_with_middle.is_a? String
-    # assert_no_match(/\s/, LeWagon::Name.name_with_middle)
-  end
-
-  def test_zh_tw_university_methods
-    # assert LeWagon::University.prefix.is_a? String
-    # assert LeWagon::University.suffix.is_a? String
-    # assert LeWagon::University.name.is_a? String
-  end
 end
