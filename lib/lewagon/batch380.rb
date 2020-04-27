752b063993e5c166d07b3050c074a2a348645fec
Daniel Polo <od.polo@gmail.com>
2020-04-26 22:31:15 -0500

    feat(refactor): Introduced YAML dash syntax

--- lib/lewagon/batch380.rb
+++ lib/lewagon/batch380.rb
@@ -3,61 +3,11 @@
 module LeWagon
   class Batch380 < Base
     class << self
-      def andrea
-        fetch('batch380.massi')
-      end
-
-      def domenico
-        fetch('batch380.domenico')
-      end
-
-      def fernando
-        fetch('batch380.fernando')
-      end
-
-      def gian_marco
-        fetch('batch380.gian_marco')
-      end
-
-      def kevin
-        fetch('batch380.kevin')
-      end
-
-      def leo
-        fetch('batch380.leo')
-      end
-
-      def mahsa
-        fetch('batch380.mahsa')
-      end
 
       def massi
         fetch('batch380.massi')
       end
 
-      def molly
-        fetch('batch380.molly')
-      end
-
-      def peter
-        fetch('batch380.peter')
-      end
-
-      def rudy
-        fetch('batch380.rudy')
-      end
-
-      def sam
-        fetch('batch380.sam')
-      end
-
-      def savina
-        fetch('batch380.savina')
-      end
-
-      def stephanie
-        fetch('batch380.stephanie')
-      end
     end
   end
 end
